7952d229c012575f5f9b06ed9a350c07eab46e6a
eduardo98m <32850455+eduardo98m@users.noreply.github.com>
2021-02-20 23:56:33 -0400

    fixed error with setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='blacky_bullet',
+      version='1.0',
+      description='A package to run reinforcement learning algorithms on pybullet',
+      author='Eduardo López and Amin Arriaga',
+      author_email='eduardo98m@gmail.com',
+      url='',
+      packages=['gym_env'],
+     )
