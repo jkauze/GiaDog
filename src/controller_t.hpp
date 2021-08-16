c22555001c569c85f09da7c2e07ff5efed527bc7
eduardo98m <32850455+eduardo98m@users.noreply.github.com>
2021-08-15 21:24:19 -0400

    Added inverse kinematiks function

--- src/controller_t.hpp
+++ src/controller_t.hpp
@@ -12,33 +12,54 @@
 #include <iostream>
 
 // Math
-#include <Eigen/Dense>
+#include <Eigen/Dense> //Matrix manipulation (numpy like arrays)
 #include <cmath>
 
-using namespace std;
 
 /*
 	[TODO: DESCRIPTION]
 */
 class controller_t {
 	private:
-		// Inverse Kinematics Variables
-		// 
-		double l_s_, l_w_; 
-		double off_0_, off_1_;
-		double h_1 = sqrt( pow(this->off_0_, 2) + pow( this-> off_1_, 2) );
+		/*
+
+			Inverse Kinematics variables
+			
+			l_s_   : double -> Lenght of the shoulder joint
+			l_w_   : double -> Lenght of the wrist joint
+			off_0_ : double -> Vertical offset betweet hip and shoulder link
+			off_1_ : double -> Horizontal offset betweet hip and shoulder link
+		
+		*/
+		
+		double l_s_, l_w_, off_0_, off_1_; 
+		
+		/*
+
+			Foot Trajectory Generator variables 
+			
+			Hi_z         : Eigen::Vector3d -> i-th leg horizontal frame z component.
+			
+			sigma_i_0_ : double -> Lenght of the wrist joint.
+			
+			h_   : double -> Maximun foot height. # Default 0.2 m
+			f_0_ : double -> Robot gait common frequency. # Default 1.25 Hz
+		
+		*/
+
+		Eigen::Vector3d Hi_z_;
+		double sigma_i_0_, h_, f_0_;
 		
 
 	public:
 		controller_t(void) {}
 		/*
+			[TO DO: DESCRIPTION]
+			
 			Args:
-				t: double  ->  Timestep.
+				t  : double  ->  Timestep.
 				f_i: double  ->  i-th leg frequency offset (from NN policy).
-				Hi_z: Eigen::Vector3d  ->  i-th leg horizontal frame z component.
-				sigma_i_0: double  ->  i-th leg initial phase.
-				h: double  ->  Maximun foot height. Set by default to 0.2.
-				f_0: double  ->  Robot gait common frequency. Set by default to 1.25.
+				
 			Return:
 				Eigen::Vector3d  ->  Vector expresed in the i-th leg horizontal frame Hi,
 					representing de target foot position.
@@ -49,27 +70,26 @@ class controller_t {
 		*/
 		Eigen::Vector3d FTG(
 			double t,
-			double f_i,
-			Eigen::Vector3d Hi_z,
-			double sigma_i_0,
-			double h = 0.2,
-			double f_0 = 1.25
-		)
+			double f_i
+			)
 		{
-			double sigma_i = (sigma_i_0 + (f_0 + f_i) * t) % (2 * M_PI);
-			double k = 2 * (sigma_i - M_PI) / M_PI;
+			const double pi = 3.141592653589793;
+			double sigma_i;
+			sigma_i = fmod(this->sigma_i_0_ + (this->f_0_ + f_i) * t, 2 * pi);
+			double k = 2 * (sigma_i - pi) / pi;
 
 			if ( 0 < k  && k < 1)
 			{
-				return (h * ( -2* pow(k, 3) + 3*pow(k,2)) - 0.5) * Hi_z;
+				return (this->h_*(-2* pow(k, 3) + 3*pow(k,2)) - 0.5) * this->Hi_z_;
 			}
 			else if (1 < k  && k < 2)
 			{
-				return (h * ( 2* pow(k, 3) - 9* pow(k, 2) +  12*k - 4) - 0.5) * Hi_z;
+				return (this->h_*(2* pow(k, 3) - 9* pow(k, 2) + 12*k - 4) - 0.5)
+						*this->Hi_z_;
 			}
 			else
 			{
-				return 0.5  * Hi_z;
+				return - 0.5 * this->Hi_z_;
 			};
 		}
 
@@ -79,19 +99,21 @@ class controller_t {
 		Eigen::Vector3d inverse_kinematics(
 								Eigen::Vector3d r
 								)
-		{ 	// Input
+		{ 	
+			// Input
 			double x = r[0], y = r[1], z = r[2];
 			// Output
 			double theta_h, theta_s, theta_w;
 
-
+			// Auxiliary Variables
 			double h_2 = sqrt( pow(z,2) + pow(y,2) );
-
 			double r_o =  sqrt( pow(h_2,2) - pow(this->off_1_,2) ) - this->off_0_ ;
-		
-			theta_h = atan(y/z) - asin(this->off_1_ /h_2);
-
 			double h = sqrt(pow(r_o,2) + pow(x,2));
+
+
+			// Joint angles calculation
+			
+			theta_h = atan(y/z) - asin(this->off_1_ /h_2);
 
 			theta_s = acos((pow(h,2) + pow(this->l_s_,2) - pow(this->l_w_,2))/
 					  (2*h*this->l_s_)) -asin(x/h);
@@ -99,7 +121,7 @@ class controller_t {
 			theta_w = acos((pow(this->l_w_,2) + pow(this->l_s_,2)-pow(h,2))/
 					  (2*this->l_w_*this->l_s_));
 
-			return (theta_h, theta_s, theta_w);
+			return Eigen::Vector3d(theta_h, theta_s, theta_w);
 		};
 };
 
